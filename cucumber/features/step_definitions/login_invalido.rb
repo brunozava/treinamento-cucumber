1cf853050bdd78ad859bb3975d691c92368f16d0
Zava <bruno_zava@hotmail.com>
2017-02-08 22:58:20 -0200

    Sei la o que aconteceu.

--- cucumber/features/step_definitions/login_invalido.rb
+++
@@ -1,9 +0,0 @@
-Quando (/^eu passar usuario e senha invalidos$/) do
-find(:css, '#email-cpf').set("bruno.zavanellasss@netshoes.com")
-find(:css, '#password').set("zava130887333")
-end
-
-Entao (/^devera ser mostrado uma mensagem que o login ou senha são invalidos$/) do
-expect(page).to have_content 'Ops! Usuário não encontrado. Confira seus dados ou crie uma conta'
-#expect(page).to have_selector :css, '.user-name'
-end
